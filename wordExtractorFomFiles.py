2a3b3f3d87ceeb76936be06ad287833d10e6f30a
rudolferemyan <eremyan.rudolf@gmail.com>
2016-06-27 19:03:49 +0400

    Extracts Georgian Words from Collection of Files

---
+++ wordExtractorFomFiles.py
@@ -0,0 +1,28 @@
+#getting list of all file names in folder
+def getFileNamesListInDirectory():
+    import glob
+    return glob.glob('./test/*.html')
+    
+    
+def readTextFile(path):
+    with open(path, 'r', encoding="utf8") as myfile:
+        data = myfile.read().replace('\n', '')
+        return data
+
+
+#TODO
+def extractGeorgianWords(s):
+    import re
+    matches = re.findall('([ხელმძღქწერტყუიოპასდფგჰჯკლზხცვბნმთშჟჩძ]*-[ქწერტყუიოპასდფგჰჯკლზხცვბნმთშჟჩძ]*)|([ხელმძღქწერტყუიოპასდფგჰჯკლზხცვბნმთშჟჩძ]*)', s, re.DOTALL)
+    res = [(' ' + str(i[1]) + ' ') for i in matches if (len(str(i[1])) > 0)]
+    return res
+ 
+def getGeoWordsFromFiles():   
+    files = getFileNamesListInDirectory()
+    for file in files:
+        doc = readTextFile(file)
+        georgianOnly = extractGeorgianWords(doc)
+        print(georgianOnly)
+
+
+
