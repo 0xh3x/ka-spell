8df4581b04ab804d275c8332d28fd9ce40504a59
rudolferemyan <eremyan.rudolf@gmail.com>
2016-06-27 20:14:38 +0400

    updated wordExtractorFomFiles.py

--- wordExtractorFomFiles.py
+++ wordExtractorFomFiles.py
@@ -1,26 +1,41 @@
+#Global Set
+D = set()
+
 #getting list of all file names in folder
-def getFileNamesListInDirectory():
-    import glob
-    return glob.glob('./test/*.html')
+def getFileNamesListInDirectory(inputPath):
+        import glob
+        # inputPath param example: './test/'
+        return glob.glob(inputPath + '*.html')
     
-    
-def readTextFile(path):
-    with open(path, 'r', encoding="utf8") as myfile:
-        data = myfile.read().replace('\n', '')
-        return data
+def readTextInFile(path):
+        with open(path, 'r', encoding="utf8") as myfile:
+            data = myfile.read().replace('\n', '')
+            return data
 
 def extractGeorgianWords(s):
-    import re
-    matches = re.findall('([ხელმძღქწერტყუიოპასდფგჰჯკლზხცვბნმთშჟჩძ]*-[ქწერტყუიოპასდფგჰჯკლზხცვბნმთშჟჩძ]*)|([ხელმძღქწერტყუიოპასდფგჰჯკლზხცვბნმთშჟჩძ]*)', s, re.DOTALL)
-    res = [(' ' + str(i[1]) + ' ') for i in matches if (len(str(i[1])) > 0)]
-    return res
+        import re
+        matches = re.findall('([ხელმძღქწერტყუიოპასდფგჰჯკლზხცვბნმთშჟჩძ]*-[ქწერტყუიოპასდფგჰჯკლზხცვბნმთშჟჩძ]*)|([ხელმძღქწერტყუიოპასდფგჰჯკლზხცვბნმთშჟჩძ]*)', s, re.DOTALL)
+        res = [(' ' + str(i[1]) + ' ') for i in matches if (len(str(i[1])) > 0)]
+        return res
  
-def getGeoWordsFromFiles():   
-    files = getFileNamesListInDirectory()
-    for file in files:
-        doc = readTextFile(file)
-        georgianOnly = extractGeorgianWords(doc)
-        print(georgianOnly)
-
-
-
+def getGeoWordsFromFiles(inputPath):   
+        files = getFileNamesListInDirectory(inputPath)
+        for file in files:
+            doc = readTextFile(file)
+            georgianOnly = extractGeorgianWords(doc)
+            for word in georgianOnly:
+                D.add(word)
+        
+#must have alreade created output file (output.txt)
+def save2file(filename):
+        import io
+        with open(filename, 'w', encoding="utf8") as f:
+           for s in D:
+               f.write(s + '\n')
+    
+#run this function to generate GeoWords DataBase from files in catalof    
+#inputPath - path to folder where located html files
+#outputPath - path to file where will have to be written extracted georgian words
+def generateGeoWordDB(inputPath, outputPath):
+    getGeoWordsFromFiles(inputPath)
+    save2file(outputPath)
